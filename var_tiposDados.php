05984ced4f1c8f0932a85a7ae08bea7b9efa801d
Larissa-alSouza <162065841+Larissa-alSouza@users.noreply.github.com>
2024-09-23 21:33:35 -0300

    Create var_tiposDados.php

---
+++ var_tiposDados.php
@@ -0,0 +1,13 @@
+<?php 
+
+// variáveis são declaradas com "$"
+
+// não é necessário espevcificar qual é o tipo de variável
+
+$nome = "João";  // String
+$idade = 25;     // Inteiro
+$salario = 2500.50;  // Float
+
+// suporta dados do tipo: int, float, string, booleanos, arrays e objetos (principais)
+
+?>
