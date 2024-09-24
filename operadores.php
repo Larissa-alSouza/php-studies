b59e61b8e2ba4938c39988884379840d04b3cb28
Larissa-alSouza <162065841+Larissa-alSouza@users.noreply.github.com>
2024-09-23 21:34:48 -0300

    Create operadores.php

---
+++ operadores.php
@@ -0,0 +1,20 @@
+<?php 
+
+// utilizados para operações matemáticas
+
+//soma (+), subtração (-), multiplicação (*), divisão (/), módulo (%)
+
+// div (/) retorna o quociente da divisão e mod(%) retorna o resto
+
+$a = 10;
+$b = 5;
+$soma = $a + $b;  // Resultado: 15
+$divisao = $a / $b;  // Resultado: 2
+
+// operadores de atribuição: igual (=), Atribuição com operação (+=, -=, *=, etc.)
+
+// operadores de comparação: Igualdade (==), Identidade (===), Diferente (!=), Maior que (>), Menor que (<)
+
+// operadores lógicos: AND (&&), OR (||), NOT (!)
+
+?>
